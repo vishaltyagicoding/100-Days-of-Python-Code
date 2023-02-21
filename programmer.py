0a3fabbe42bc798d8dafab2791629fd9f32f4ad1
Vishal Tyagi <124853710+vishaltyagicoding@users.noreply.github.com>
2023-02-21 09:49:49 +0530

    Add files via upload

---
+++ programmer.py
@@ -0,0 +1,11 @@
+because_im_a_programmer =\
+    "❤️❤️❤️ I LOVE CODING ❤️❤️❤️"
+
+what_do_you_do_for_living = "I'm just breathing"
+
+until_i_die = 100  # years
+print(what_do_you_do_for_living)
+
+for i_need_coffee in range(until_i_die):
+    print(because_im_a_programmer)
+
